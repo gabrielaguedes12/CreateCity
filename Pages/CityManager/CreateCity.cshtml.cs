d3d323b74f520cd5ec71b70fe1fd29a8749490c7
gabrielaguedes12 <gabr1.profissional@gmail.com>
2026-03-11 14:23:17 -0300

    subindo ajustes

--- Pages/CityManager/CreateCity.cshtml.cs
+++ Pages/CityManager/CreateCity.cshtml.cs
@@ -1,12 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CreateCity.Pages.CityManager
+namespace TourismApp.Pages.CityManager
 {
     public class CreateCityModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public string? CityName { get; set; }
+
+        public void OnGet() { }
+
+        public void OnPost()
         {
+            // o Model Binding já coloca o valor digitado em CityName
         }
     }
 }
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CreateCity.Pages
+namespace TourismApp.Pages
 {
     public class IndexModel : PageModel
     {
