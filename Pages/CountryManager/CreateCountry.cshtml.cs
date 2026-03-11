c688c6d7fa470e2abb87f3660ccb6efc9b364f9c
gabrielaguedes12 <gabr1.profissional@gmail.com>
2026-03-11 13:02:49 -0300

    tentando

---
+++ Pages/CountryManager/CreateCountry.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CreateCity.Pages.CountryManager
+{
+    public class CreateCountryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
