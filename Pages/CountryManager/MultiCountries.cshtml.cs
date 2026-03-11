aadc2d8810472fbc3521b795569e2cb80b521689
gabrielaguedes12 <gabr1.profissional@gmail.com>
2026-03-11 13:11:53 -0300

    subindo estrutura

---
+++ Pages/CountryManager/MultiCountries.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CreateCity.Pages.CountryManager
+{
+    public class MultiCountriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Notes/SaveNote.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CreateCity.Pages.Notes
+{
+    public class SaveNoteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
